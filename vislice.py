49c553f402e018294732f290f70037dc922e0522
Nika Pavlič <nika.pavlic775@gmail.com>
2021-05-04 20:27:11 +0200

    vislice3

--- vislice.py
+++ vislice.py
@@ -9,6 +9,26 @@ vislice = model.Vislice()
 def index():
     return bottle.template('index.tpl')
 
+
+@bottle.post('/igra/')
+def nova_igra():
+    id_igre = vislice.nova_igra()
+    bottle.redirect('/igra/{}/'.format(id_igre)) 
+
+
+@bottle.get('/igra/<id_igre:int>/')
+def pokazi_igro(id_igre):
+    igra, stanje = vislice.igre[id_igre]
+    return bottle.template('igra.tpl', igra = igra, id_igre = id_igre, stanje = stanje)
+
+
+@bottle.post('/igra/<id_igre:int>/')
+def ugibaj(id_igre):
+    crka = bottle.request.forms.getunicode('crka')
+    vislice.ugibaj(id_igre, crka)
+    bottle.redirect('/igra/{}/'.format(id_igre))
+
+
 @bottle.get('/img/<picture>')
 def serve_picture(picture):
     return bottle.static_file(picture, root = 'img')
