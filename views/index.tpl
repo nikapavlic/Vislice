94a64e4b2caf6fa52f89698438f4580ea7332f9a
Nika Pavlič <nika.pavlic775@gmail.com>
2021-05-11 15:10:20 +0200

    piskotki

--- views/index.tpl
+++ views/index.tpl
@@ -9,6 +9,6 @@
 
   <img src="img/10.jpg" alt="obesanje">
 
-  <form action="/igra/" method="post">
+  <form action="/nova-igra/" method="post">
     <button type="submit">Nova igra</button>
   </form>
--- vislice.py
+++ vislice.py
@@ -1,6 +1,8 @@
 import bottle
 import model
 
+SKRIVNOST = 'moja skrivnost'
+
 #treba pisat model.Vislice() drugače neve od kod je
 vislice = model.Vislice()
 
@@ -10,23 +12,26 @@ def index():
     return bottle.template('index.tpl')
 
 
-@bottle.post('/igra/')
+@bottle.post('/nova-igra/')
 def nova_igra():
     id_igre = vislice.nova_igra()
-    bottle.redirect('/igra/{}/'.format(id_igre)) 
+    bottle.response.set_cookie('idigre','idigre{}'.format(id_igre), path='/', secret = SKRIVNOST)
+    bottle.redirect('/igra/') 
 
 
-@bottle.get('/igra/<id_igre:int>/')
-def pokazi_igro(id_igre):
+@bottle.get('/igra/')
+def pokazi_igro():
+    id_igre = int(bottle.request.get_cookie('idigre', secret= SKRIVNOST).split('e')[1])
     igra, stanje = vislice.igre[id_igre]
-    return bottle.template('igra.tpl', igra = igra, id_igre = id_igre, stanje = stanje)
+    return bottle.template('igra.tpl', igra = igra, stanje = stanje)
 
 
-@bottle.post('/igra/<id_igre:int>/')
-def ugibaj(id_igre):
+@bottle.post('/igra/')
+def ugibaj():
+    id_igre = int(bottle.request.get_cookie('idigre', secret= SKRIVNOST).split('e')[1])
     crka = bottle.request.forms.getunicode('crka')
     vislice.ugibaj(id_igre, crka)
-    bottle.redirect('/igra/{}/'.format(id_igre))
+    bottle.redirect('/igra/')
 
 
 @bottle.get('/img/<picture>')
